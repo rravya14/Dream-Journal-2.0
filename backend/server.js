61481a6a7ae7a05f7c7fb36f0f0efb95b70e4d97
rravya14 <bhavyapunj364@gmail.com>
2025-11-12 01:01:52 +0530

    added frontend files

--- backend/server.js
+++ backend/server.js
@@ -13,7 +13,7 @@ app.use(express.json());
 app.use(cookieParser());
 app.use(express.urlencoded({ extended: true }));
 app.use(cors({
-  origin: [`http://localhost:3000`],
+  origin: [`http://localhost:3000`, process.env.FRONTEND_URL],
   credentials: true,
 })
 );
